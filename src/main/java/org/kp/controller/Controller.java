b0a4294da03f87397bc8fae871c73dc8209ade79
ypenn21 <yannipeng21@gmail.com>
2019-03-26 14:35:35 -0700

    added log added new rest end point

--- src/main/java/org/kp/controller/Controller.java
+++ src/main/java/org/kp/controller/Controller.java
@@ -1,13 +1,17 @@
 package org.kp.controller;
 
+import org.kp.processors.Processor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+
+    private Logger logger = Logger.getLogger(Processor.class.getName());
 
     @RequestMapping(value = "/json/test", method = RequestMethod.GET)
     @ResponseBody
@@ -20,12 +24,23 @@ public class Controller {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Map greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    @RequestMapping(value = "/environment/test", method = RequestMethod.GET)
+    @ResponseBody
+    public Map testEnvironment() {
         String test = System.getenv("test");
+        logger.info("environment variable is: "+test);
         Map mymap = new HashMap();
-        mymap.put(counter.incrementAndGet(), String.format(template, name));
         mymap.put("test", test);
+        return mymap;
+    }
+
+    @RequestMapping("/greeting/test")
+    @ResponseBody
+    public Map greeting(@RequestParam(value="name", defaultValue="World") String name) {
+//        String test = System.getenv("test");
+//        logger.info("environment variable is: "+test);
+        Map mymap = new HashMap();
+        mymap.put(counter.incrementAndGet()+"", String.format(template, name));
         return mymap;
     }
 }
